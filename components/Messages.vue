118958fa985ee3026dba72169873b60f7cfe56eb
Hiroyuki Ikuta <i.hiro.0513@gmail.com>
2018-09-28 00:15:06 +0900

    messagesの要素だけでスクロールできるように

--- components/Messages.vue
+++ components/Messages.vue
@@ -1,13 +1,15 @@
 <template>
-  <div>
-    <!--TODO:投稿がゼロ件でもloadingになってしまう-->
-    <div v-if="messages.length > 0">
-      <div v-for="message in messages">
-        <message :message=message></message>
+  <div class="messages-container">
+    <div class="messages">
+      <!--TODO:投稿がゼロ件でもloadingになってしまう-->
+      <div v-if="messages.length > 0">
+        <div v-for="message in messages">
+          <message :message=message></message>
+        </div>
       </div>
-    </div>
-    <div v-else>
-      loading messages...
+      <div v-else>
+        loading messages...
+      </div>
     </div>
   </div>
 </template>
@@ -32,3 +34,12 @@
     }
   }
 </script>
+
+<style scoped lang="sass">
+  .messages-container
+    height: 50vh
+    display: flex
+    flex-direction: column
+  .messages
+    overflow-y: scroll
+</style>
